994aedbbef87456303aa3ec119ac62fdacf435fb
Rishabh Srivastava <f2013674@goa.bits-pilani.ac.in>
2017-11-22 11:47:12 +0530

    Update json_handler.py

--- json_handler.py
+++ json_handler.py
@@ -1,28 +1,18 @@
 from __future__ import print_function
 import json
+def getScores(payload):
+    scoreDict = {"skill":0,"exp":0,"Crev":0,"CErev":0,"NeoApi":0}
+    # JSON requires double-quotes, not single-quotes.
+    payload = json.loads(payload)
+    print(payload)
+    for doc in payload['payload']:
+        scoreDict[doc['type']] = doc['score']
+        print(doc['score'], doc['type'])
+    scoreList = []
+    scoreList.append(scoreDict["skill"])
+    scoreList.append(scoreDict["exp"])
+    scoreList.append(scoreDict["Crev"])
+    scoreList.append(scoreDict["CErev"])
+    scoreList.append(scoreDict["NeoApi"])
+    return scoreList
 
-response = """{
-    'ok': 1.0, 
-    'result': [
-        {
-            'total': 142250.0, 
-            '_id': 'BC'
-        }, 
-        {
-            'total': 210.88999999999996,
-             '_id': 'USD'
-        }, 
-
-        {
-            'total': 1065600.0, 
-            '_id': 'TK'
-        }
-        ]
-}"""
-
-# JSON requires double-quotes, not single-quotes.
-response = response.replace("'", '"')
-response = json.loads(response)
-print(response)
-for doc in response['result']:
-    print(doc['_id'], doc['total'])
