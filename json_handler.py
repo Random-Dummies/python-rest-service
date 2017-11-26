f7423b4cd4343a7104b70af3d63d23730cdb3cc4
Rishabh Srivastava <f2013674@goa.bits-pilani.ac.in>
2017-11-27 00:19:17 +0530

    Update json_handler.py

--- json_handler.py
+++ json_handler.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import json
 def getScores(payload):
-    scoreDict = {"skill":0,"exp":0,"Crev":0,"CErev":0,"NeoApi":0}
+    scoreDict = {"skills":0,"experiance":0,"CompanyReview":0,"CEOReview":0,"NeoApi":0}
     # JSON requires double-quotes, not single-quotes.
     payload = json.loads(payload)
     print(payload)
@@ -9,10 +9,10 @@ def getScores(payload):
         scoreDict[doc['type']] = doc['score']
         print(doc['score'], doc['type'])
     scoreList = []
-    scoreList.append(scoreDict["skill"])
-    scoreList.append(scoreDict["exp"])
-    scoreList.append(scoreDict["Crev"])
-    scoreList.append(scoreDict["CErev"])
+    scoreList.append(scoreDict["skills"])
+    scoreList.append(scoreDict["experiance"])
+    scoreList.append(scoreDict["CompanyReview"])
+    scoreList.append(scoreDict["CEOReview"])
     scoreList.append(scoreDict["NeoApi"])
     return scoreList
 
